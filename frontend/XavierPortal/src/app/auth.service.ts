db566f5ac4b558d909afcf60229c02c962d7ecf5
geofpwhite <108630660+geofpwhite@users.noreply.github.com>
2023-09-11 13:46:08 -0400

    adding role enum

--- frontend/XavierPortal/src/app/auth.service.ts
+++ frontend/XavierPortal/src/app/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Role } from './enums/role';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  loginUsername: string = "";
+  userRole?: Role;
+  authorized: boolean = false;
   constructor() { }
 }
