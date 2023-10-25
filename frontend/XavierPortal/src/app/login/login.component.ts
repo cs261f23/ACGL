10576b71bbd80cf3687fe3d2f6fd6637302b3f7e
unknown <geoffpiercewhite@gmail.com>
2023-10-25 19:01:40 -0400

    login fitted to forms module better

--- frontend/XavierPortal/src/app/login/login.component.ts
+++ frontend/XavierPortal/src/app/login/login.component.ts
@@ -3,16 +3,21 @@ import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { ApiCallService } from '../api-call.service';
 import { AuthService } from '../auth.service';
 import { CommunityPartnerViewComponent } from '../community-partner-view/community-partner-view.component';
+interface user {
+  email: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css', '../material-icons.css']
 })
+
 export class LoginComponent {
 
-  email: string = '';
-  password: string = '';
+  user: user = { email: '', password: '' };
   showPassword = false;
+  submitted: boolean = false;
   loggedIn: boolean = false;
   role: number = -1;
   constructor(private apiCallService: ApiCallService, private authService: AuthService, private router: Router, private route: ActivatedRoute) {
@@ -20,7 +25,7 @@ export class LoginComponent {
   }
 
   login(): void {
-    this.apiCallService.attemptLogin(this.email, this.password).subscribe((response: any) => {
+    this.apiCallService.attemptLogin(this.user.email, this.user.password).subscribe((response: any) => {
       if (response["outcome"] == "failed") {
         return
       }
