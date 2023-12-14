e5d0f27e5d9be579f7a17d9d35dd03bbfa467985
geofpwhite <87089088+geoffreypwhite@users.noreply.github.com>
2023-12-14 18:52:57 -0500

    _

--- backendGo/app/models/student/student.go
+++ backendGo/app/models/student/student.go
@@ -10,6 +10,24 @@ import (
 
 // student
 
+/*
+CREATE TABLE `students` (
+
+	`id` int DEFAULT NULL,
+	`name` varchar(50) NOT NULL,
+	`student_email` varchar(254) NOT NULL,
+	`student_id` int NOT NULL AUTO_INCREMENT,
+	`password` varchar(256) DEFAULT NULL,
+	`salt` blob,
+	`deleted_at` time DEFAULT NULL,
+	`created_at` time DEFAULT NULL,
+	`updated_at` time DEFAULT NULL,
+	PRIMARY KEY (`student_id`),
+	UNIQUE KEY `unique_student_id` (`student_id`),
+	KEY `id` (`id`)
+
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+*/
 type Student struct {
 	gorm.Model
 	Name         string
